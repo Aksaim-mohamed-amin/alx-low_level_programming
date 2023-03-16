c3a19d031f17bf6adc7e8e6da45356013fd1bd77
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-16 15:35:49 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#includ < stdio.h >
+/**
+ * print a string using puts function
+ * and returning 0
+ */
+/* main function */
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(string);
+	/* return 0*/
+	return (0);
+}
