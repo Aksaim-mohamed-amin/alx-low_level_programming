d0092662d6bfc79f431314584e1a224c374c5e1f
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-17 15:44:59 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry piont
+ *
+ * Description: 'print all the single digit nubers of base 10'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
