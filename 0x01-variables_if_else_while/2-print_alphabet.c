ea4e3cd552563e110783311ad05a73e90f16e16c
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-17 13:27:40 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry piont
+ *
+ * Description: 'print the alphabit in lowercase using putchar'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghigklmnopqrstuvwz";
+
+	for (int i = 0; i < sizeof(alphabet) - 1; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
