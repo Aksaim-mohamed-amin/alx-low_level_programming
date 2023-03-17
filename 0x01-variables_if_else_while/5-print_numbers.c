186d027a11dc813c6755f1c5a8b2295bcc6a3c36
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-17 15:36:22 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry piont
+ *
+ * Description: 'print all the single digit nubers of base 10'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
