8d99176bb90907d1f0526282155d4349da62f428
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-16 17:31:27 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,10 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\""
-"Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	const char msg1[] = "and that piece of art is useful\" - ";
+	const char msg2[] = "Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, msg1, sizeof(msg1) - 1);
+	write(STDERR_FILENO, msg2, sizeof(msg2) - 1);
 	return (1);
 }
