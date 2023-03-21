ed380931883677abd100f337b0af304f9352852b
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-21 15:42:00 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry piont
  *
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar();
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void _putchar(void)
+{
+	printf("_putchar\n");
+}
