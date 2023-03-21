80980a32207ed8bbbf88c15148e73f319976d76e
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-21 12:47:06 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ *
+ * Description: 'print _putchar'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
