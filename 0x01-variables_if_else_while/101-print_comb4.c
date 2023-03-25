08d87c9e6246ee020ed4e4dea91cedbdeab10b87
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-25 16:56:38 -0700

    mandatory task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print all possibe combinations of three digit numbers
+ *  using putchar'
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
