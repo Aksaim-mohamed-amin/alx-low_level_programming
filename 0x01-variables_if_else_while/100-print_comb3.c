01e4cadc1b24daa2f5e619368fa62bf808ff6697
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-25 16:25:45 -0700

    mandatory task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print all possibe combinations of two digit numbers
+ *  using putchar'
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			if (i < j)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
