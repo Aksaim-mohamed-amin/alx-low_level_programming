3db96fe08f4faa2e9b07438ad661a904e6c8b916
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-25 15:18:04 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print the alphabet using putchar()'
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a', letter < 'z', letter++)
+	{
+		putchar('0' + letter);
+	}
+	putchar('\n');
+	return (0);
+}
