df67e8b115c9b8ec0d767102d72d021d08b0ece4
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-25 19:35:25 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,22 @@
 
 int print_sign(int n)
 {
+	int r;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
-	} else if (n == 0)
+		r = -1;
+	}
+	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
-	} else if (n > 0)
+		r = 0;
+	}
+	else if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		r = 1;
 	}
+	return (r);
 }
