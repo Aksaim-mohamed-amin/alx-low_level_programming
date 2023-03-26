15caf5cd6575644ee16ded1c2547c6e67543dcbe
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-26 11:15:28 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - Main function
+ *
+ * Description: 'return the sum of two integers'
+ *
+ * @x: number to add
+ * @y: number to add
+ *
+ * Return: the summe of x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
