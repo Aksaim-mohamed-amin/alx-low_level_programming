3210042cae16b07ddde544495b2c1fc2625fa653
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-26 16:29:36 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'computes and prints the sum of all the miltiples
+ * of 3 and 5 below 1024 (excluded)
+ */
+
+void main(void)
+{
+	int x, sum;
+
+	x = 1023;
+	sum = 0;
+	for (; x > 0; x--)
+	{
+		if (x % 5 == 0 || x % 3 == 0)
+		{
+			sum += x;
+		}
+	}
+	printf("%d\n", sum);
+}
