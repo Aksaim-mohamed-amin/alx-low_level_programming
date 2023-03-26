569e8e873d62f04d3090fc268eae609b3d992924
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-26 13:56:54 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+/**
+ * print_to_98 - main function
+ *
+ * Description: 'Print to 98 from any giving number'
+ *
+ * @n: the number to start printing from
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (;n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (;n > 98; n--)
+		{
+			printf("%d, " ,n);
+		}
+	}
+	printf("98\n");
+}
