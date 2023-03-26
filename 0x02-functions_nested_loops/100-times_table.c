623056da6982fbaa65ca8082d62dd64f2e3c3a1d
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-26 16:03:37 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * times_table - Main function
+ *
+ * Description: 'print the 9 times table starting with 0'
+ *
+ * @n: the number of tables
+ */
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				k = i * j;
+
+				if (k < 10)
+				{
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + k);
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + k / 10);
+					_putchar('0' + k % 10);
+				}
+				else if (k >= 100)
+				{
+					_putchar('0' + k / 100);
+					_putchar('0' + (k / 10) % 10);
+					_putchar('0' + k % 10);
+				}
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
