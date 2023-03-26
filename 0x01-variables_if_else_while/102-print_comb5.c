7fbf860b80189513d08942913dc38f13c81f7e9c
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-25 17:03:05 -0700

    Mandatory task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'Print all possibe combinations of two digit numbers
+ * Description: 'Print all possibe combinations of four digit numbers
  *  using putchar'
  *
  * Return: Always 0
