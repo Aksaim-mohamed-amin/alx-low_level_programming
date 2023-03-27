4130348e518e2ed7c50ddcb51f7c6a7261c2656f
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-26 17:46:56 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - entrey point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long a, a2, a3, sum1;
+	unsigned long b, b2, b3, sum2;
+	int i;
+
+	printf("1, 2");
+	for (a = 1, b = 2, i = 1; i < 90; i++)
+	{
+		sum1 = a + b;
+		printf(", %ld", sum1);
+		a = b;
+		b = sum1;
+	}
+	printf(", ");
+	a2 = a % 1000;
+	a = a / 1000;
+	b2 = b % 1000;
+	b = b / 1000;
+	for (; i < 97; i++)
+	{
+		sum2 = (a2 + b2) / 1000;
+		a3 = (a2 + b2) - sum2 * 1000;
+		b3 = (a + b) + sum2;
+		a2 = b2;
+		b2 = a3;
+		a = b;
+		b = b3;
+		if (a3 >= 100)
+			printf("%lu%lu", b3, a3);
+		else
+			printf("%lu0%lu", b3, a3);
+		if (i != 96)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
