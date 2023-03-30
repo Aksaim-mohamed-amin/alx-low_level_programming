a854494c23a3832726233d34405b7ec825672bf7
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-30 08:42:22 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,9 +18,10 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		length++;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[length + i] = src[i];
 	}
+	dest[length + i] = '\0';
 	return (dest);
 }
