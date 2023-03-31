37d27322e8d8efbe4cb3f7aa85185fbeffb081cf
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-31 08:09:06 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,32 +11,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int length1 = 0, length2 = 0;
-	int length = 0;
-
-	for (i = 0; s1[i] != '\0'; i++)
+	while(*s1 && (*s1 == *s2))
 	{
-		length1++;
+		s1++;
+		s2++;
 	}
-	for (i = 0; s2[i] != '\0'; i++)
-	{
-		length2++;
-	}
-	if (length1 >= length2)
-	{
-		length = length1;
-	}
-	else
-	{
-		length = length2;
-	}
-	for (i = 0; i < length; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-	}
-	return (0);
+	return (*(char *)s1 - *(char *)s2);
 }
