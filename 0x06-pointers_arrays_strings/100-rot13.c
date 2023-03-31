27363ca72ed42a17d51da8bdfd6f44d7d9634a45
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-03-31 12:47:56 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+/**
+ * rot13 -main function
+ *
+ * Description: 'Encodes a string using rot13'
+ *
+ * @s: String to encode
+ *
+ * Return: coded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char letter[] = "abcdefghijklmnopqrstuvwxyz";
+	char code[] = "nopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letter[j] != '\0'; j++)
+		{
+			if (s[i] == letter[j])
+			{
+				s[i] = code[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
