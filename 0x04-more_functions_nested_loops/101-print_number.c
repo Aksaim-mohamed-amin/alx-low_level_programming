a6e59b668402f95ebabf8df500d1c4733977200c
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-01 14:47:36 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,16 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -n;
 	}
-	if ((n / 10) > 0)
+	if ((num / 10) > 0)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
