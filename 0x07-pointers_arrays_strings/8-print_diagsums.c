31e964429e565687fd565430e6eefe8e26871e97
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-06 16:02:11 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - main function
+ *
+ * Description: 'print the sum of two diagonals of a square matrix of intigers'
+ *
+ * @a: matrix
+ * @size: size of the matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int diag1 = 0, diag2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				diag1 += *(a + i * size + j);
+			}
+			if (i + j == size)
+			{
+				diag2 += *(a + i * size + j);
+			}
+		}
+	}
+	printf("%d, %d\n", diag1, diag2);
+}
