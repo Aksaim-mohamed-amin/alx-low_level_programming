d2b40b649465d6d4eefacbbd38a03bd2a6c008b4
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-06 06:38:27 -0700

    updating task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,11 +16,18 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == c && c != '\0')
+		if (*s == c)
 		{
-			return (s);
+			break;
 		}
 		s++;
 	}
-	return (NULL);
+	if (*s == c && c != '\0')
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
