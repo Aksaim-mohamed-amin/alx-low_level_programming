b7440e030f746e49b9b2eb7f6004c40b8eec1765
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-06 15:13:40 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -25,7 +25,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[i + j])
+			if (needle[j] != haystack[i + j])
 			{
 				break;
 			}
