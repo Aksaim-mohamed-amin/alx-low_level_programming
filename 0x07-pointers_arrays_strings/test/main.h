bc75005962f210e4302424eae918113b3b4697ce
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-06 05:59:59 -0700

    updating readme file

--- 0x07-pointers_arrays_strings/test/main.h
+++ 0x07-pointers_arrays_strings/test/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -8,7 +11,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
