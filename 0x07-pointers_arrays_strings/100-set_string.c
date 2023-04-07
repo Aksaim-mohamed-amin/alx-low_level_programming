a40a9efbbccd8c897ec7bc1b79a2c6fe28267881
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-06 19:15:47 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+/**
+ * set_string - main function
+ *
+ * Description: 'Sets the value of a pointer to a char'
+ *
+ * @s: string set
+ * @to: value to set the string s to
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
