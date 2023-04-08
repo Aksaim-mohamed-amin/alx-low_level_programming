965673180fdb32a2e39dafbe39fc5eb50ea83bef
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-08 13:17:00 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,56 @@
-/**
- *
- */
+#include "main.h"
 
-int _is_prime_number(int n)
+/**
+* is_prime_number - main function
+*
+* Description: 'take and integer and check if it is prime by testing if it is
+* even or not, if it is odd it called the function is_prime
+*
+* @n: number you want to test
+*
+* Return: 0 if it is not prime and 1 if is prime
+*/
+
+int is_prime_number(int n)
 {
-	if (n < 0)
+	if (n <= 1 || (n % 2 == 0) && n != 2)
 	{
 		return (0);
 	}
-	return (is_prime(n, 1));
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime(n, n - 2));
+	}
 }
 
 /**
+ * is_prime - secendary function
  *
+ * Description: 'get call from the main function if n is odd and it test
+ * n if it is prime or not
+ *
+ * @n: number to test
+ * @i: increment number
+ *
+ * Return: 0 if not prime 1 if it is prime
  */
 
-int is_prime( int n, int i)
+int is_prime(int n, int i)
 {
-	if (n < 
+	if (i <= 1)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	else if (n % i != 0)
+	{
+		return (is_prime(n, i - 1));
+	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_root(int n, int i);
 int is_prime_number(int n);
+int is_prime(int n, int i);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
