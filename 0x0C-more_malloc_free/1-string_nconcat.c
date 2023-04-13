6af205f8c451bec913bc9c4697ea7839b1153957
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-13 14:34:42 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,16 +18,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 
 	if (s1 == NULL)
-		s1 = "";
+		len1 = 0;
+	else
+		for (i = 0; s1[i] != '\0'; i++)
+			len1++;
 
 	if (s2 == NULL)
-		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-		len1++;
-
-	for (i = 0; s2[i] != '\0'; i++)
-		len2++;
+		len2 = 0;
+	else
+		for (i = 0; s2[i] != '\0'; i++)
+			len2++;
 
 	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (str == NULL)
@@ -38,6 +38,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < n && i < len2; i++)
 		str[len1 + i] = s2[i];
+
+	str[len1 + len2] = '\0';
 
 	return (str);
 }
