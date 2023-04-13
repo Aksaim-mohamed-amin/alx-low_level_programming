11b2304febb09f920d47900504a4c1b3a5561d95
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-12 19:44:50 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,22 +1,53 @@
+#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * strtow - main function
+ * strtow - splits a string into words.
  *
- * Desciption: 'Split a string into words'
+ * @str: string
  *
- * @str: string passed to the function
- *
- * Return: a pointer to an array of string of words
+ * Return: pointer to array of strings
  */
 
 char **strtow(char *str)
 {
-	int i, len = 0, words = 0;
+	char **p;
+	int i, j, k, words, len;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0, words = 0; str[i] != '\0'; i++)
 	{
-		len++;
-		word
+		if (str[i] == ' ' && (i != 0 && str[i - 1] != ' '))
+			words++;
 	}
+	if (str[i - 1] != ' ' && words != 0)
+		words++;
+	p = malloc(sizeof(char *) * (words + 1));
+	if (p == NULL || words == 0)
+		return (NULL);
+	for (i = 0, k = 0; k < words && str[i] != '\0'; k++)
+	{
+		if (str[i] != ' ')
+		{
+			for (len = 0; str[i + len] != ' ' && str[i + len] != '\0'; len++)
+				;
+			p[k] = malloc(sizeof(char) * (len + 1));
+			if (p[k] == NULL)
+			{
+				for (; k >= 0; k--)
+					free(p[k]);
+				free(p);
+				return (NULL);
+			}
+			for (j = 0; j < len; j++, i++)
+				p[k][j] = str[i];
+			p[k][j] = '\0';
+		}
+		else
+		{
+			i++;
+			k--;
+		}
+	}
+	return (p);
 }
