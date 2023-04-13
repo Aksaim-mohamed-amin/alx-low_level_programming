164aa4ac2ce47fe6d3d6e5412a615afb3ff3c98c
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-12 19:22:13 -0700

    task 5

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * strtow - main function
+ *
+ * Desciption: 'Split a string into words'
+ *
+ * @str: string passed to the function
+ *
+ * Return: a pointer to an array of string of words
+ */
+
+char **strtow(char *str)
+{
+	int i, len = 0, words = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+		word
+	}
+}
