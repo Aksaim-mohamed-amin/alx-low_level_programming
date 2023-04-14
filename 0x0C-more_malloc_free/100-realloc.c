59cd15025296bc0b0b46c2e9ceef69b0022a8818
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-14 10:47:59 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	new_ptr = malloc(sizeof(char) * new_size);
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	new_ptr = malloc(new_size);
 
 	if (new_ptr == NULL)
 		return (NULL);
