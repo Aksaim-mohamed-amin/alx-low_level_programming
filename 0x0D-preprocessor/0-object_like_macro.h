aa89ce2d9b6403d00f9f93b58fb637103f699e17
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-17 17:20:51 -0700

    task 1, 2, 3 and 4

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* MAIN_H */
+#endif /* OBJECT_LIKE_MACRO_H */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(X, Y) ((X) + (Y))
+
+#endif /* SUM_H */
