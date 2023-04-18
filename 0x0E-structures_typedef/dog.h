470c4175de62570f2b42e9a9013c6c4a3300b8b1
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-17 19:14:24 -0700

    task 0 and readme file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ *
+ * Description: a struct representing a dog and its attributes.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
