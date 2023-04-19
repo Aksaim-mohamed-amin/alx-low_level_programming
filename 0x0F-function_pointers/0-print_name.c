662baf7605a2e3ec1379bd94eccfc3d70a46938d
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-19 16:31:00 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
+#include <stddef.h>
+
 /**
  * print_name - main function
  * Description: Function pointer to print a name
  * @name: name to print
  * @f: function used to print the name
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL)
+		f(name);
 }
