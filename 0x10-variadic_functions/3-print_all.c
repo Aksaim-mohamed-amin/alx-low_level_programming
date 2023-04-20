03207d4c5e9aee7315a4d43cf42f51edccc49f8d
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-20 16:25:51 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_all - main function
+ * Description: print all type of data
+ * @format: format to print
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str;
+
+	va_list data;
+
+	va_start(data, format);
+
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(data, int));
+			break;
+
+		case 'i':
+			printf("%d", va_arg(data, int));
+			break;
+
+		case 'f':
+			printf("%f", va_arg(data, double));
+			break;
+
+		case 's':
+			str = va_arg(data, char *);
+			printf("%s", str != NULL ? str : "(nil)");
+			break;
+
+		default:
+			i++;
+			continue;
+		}
+
+		if (format[i + 1] != '\0')
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+
+	va_end(data);
+}
