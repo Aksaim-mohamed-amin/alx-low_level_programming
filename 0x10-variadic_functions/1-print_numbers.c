4cbd5ac2568a8c2fdbcea1ab154302935c0eb8b8
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-04-20 08:31:48 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,28 @@
-mlskdml
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - main function
+ * Description: print numbers follwed by a new line
+ * @seperator: what to seperate each numbers with
+ * @n: number of numbers
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	/* Create the variadic list */
+	va_list nums;
+
+	/* Initialize the variadic list */
+	va_start(nums, n);
+
+	/* Print the numbers */
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(nums, int), seperator);
+
+	printf("%d\n", va_arg(nums, int));
+
+	/* Clear the variadic list */
+	va_end(nums);
+}
