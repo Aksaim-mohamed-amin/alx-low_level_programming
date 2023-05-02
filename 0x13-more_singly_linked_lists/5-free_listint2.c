1609c98cd36bda60cf17835b4375586aa9e2494b
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-05-02 02:34:28 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,7 +5,7 @@
  *
  * @head: The head of the list
  */
-void free_listint(listint_t **head)
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
@@ -14,8 +14,8 @@ void free_listint(listint_t **head)
 
 	while (*head != NULL)
 	{
-		temp = *(head)->next;
-		free(head);
+		temp = (*head)->next;
+		free(*head);
 		*head = temp;
 	}
 
