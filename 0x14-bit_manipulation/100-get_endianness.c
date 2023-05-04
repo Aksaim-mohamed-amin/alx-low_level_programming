c436c0c646f9d5c6bc37603ba6d8fa0b8d2cf37d
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-05-04 16:29:32 -0700

    task 5 and 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,8 +7,8 @@
  */
 int get_endianness(void)
 {
-        unsigned int x = 1;
-        char *c = (char *)&x;
+	unsigned int x = 1;
+	char *c = (char *)&x;
 
-        return (int)*c;
+	return ((int)*c);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,13 +11,13 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int diff = n ^ m;
-        unsigned int count = 0;
+	unsigned int count = 0;
 
-        while (diff)
-        {
-                count += diff & 1;
-                diff >>= 1;
-        }
+	while (diff)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
 
-        return (count);
+	return (count);
 }
