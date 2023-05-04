05d4ecd32166307ff998d19ca72c084597f7c875
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-05-04 15:25:58 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,17 +9,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask = 1;
-
-	mask <<= index;
-
-	if (n == NULL || index == NULL)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	if (mask & n)
-		return (1);
-	else
-		return (0);
-
-	return (-1);
+	return ((n >> index) & 1);
 }
