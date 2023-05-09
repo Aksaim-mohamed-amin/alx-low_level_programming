e1c4e2a13f3e54f8348f41e78fef06fbe1c32561
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-05-09 10:51:52 -0700

    task 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,9 @@
 
 /* Main Functions */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+/* Help Functions */
+int str_len(char *str);
 
 #endif /* MAIN_H */
