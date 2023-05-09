63c5ece9e7b40136effd7f6b4e4c72f9b94e2b75
Akasim-Mohamed-Amin <aksaimmohamedamin@gmail.com>
2023-05-09 08:50:27 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,34 @@
-/*
- * File: 0-read_textfile.c
- * Auth: Dr Marcus.
- */
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - Read a text file and prints it to the POSIX standard outpu
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * @filename: Pointer to the file name
+ * @letters: The number of letters that it should be read and print
+ * Return: 0 If the function fails or filename is NULL, else return the number
+ *         of character printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char *buffer;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
 	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	r = read(o, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (o == -1 || r == -1 || r == -1 || r != w)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
 
-	free(buffer);
+	free(buf);
+
 	close(o);
 
-	return (w);
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,21 +1,13 @@
 #ifndef MAIN_H
-#define MAIN_h
+#define MAIN_H
 
 /* Libraries */
-#include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <usistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /* Main Functions */
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
 
-/* Help Functions */
-
-
-#endif /*  MAIN_H */
+#endif /* MAIN_H */
