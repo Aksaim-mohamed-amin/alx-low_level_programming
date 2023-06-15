da376c282157d33fcf0c90aa264a5d20ae3639d9
Aksaim Mohamed Amin <Aksaimmohamedamin@Xgmail.com>
2023-06-15 06:51:37 -0700

    fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,10 +4,13 @@
  * malloc_checked - Allocates memory using malloc and exit with 98 if it fails.
  *
  * @b: Number of bytes to allocate.
+ *
+ * Return: Pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
+
 	if (ptr == NULL)
 		exit(98);
 
