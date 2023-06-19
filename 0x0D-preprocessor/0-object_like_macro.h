faafc60cdf27cf6325dfa1d779cbfbbe3e921de2
Aksaim Mohamed Amin <Aksaimmohamedamin@Xgmail.com>
2023-06-19 04:14:34 -0700

    all tasks

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/* Macros */
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H */
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#ifndef PI_H
+#define PI_H
+
+/* Macros */
+#define PI 3.14159265359
+
+#endif /* PI_H */
