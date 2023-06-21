ddf8a1d119e15c124e9475e8ebebded78ebe9a71
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-06-21 14:31:36 -0700

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (action != NULL)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
