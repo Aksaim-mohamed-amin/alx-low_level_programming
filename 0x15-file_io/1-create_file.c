e5b2919ab9fcf33ccbb6d45745076545ab02bbab
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-07-10 16:55:33 -0700

    all

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with the specified filename and writes the
+ *               text content to it.
+ *
+ * @filename: The name of the file to create.
+ * @text_content: A NULL-terminated string to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, r_value;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		r_value = write(file, text_content, str_len(text_content));
+		if (r_value == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	close(file);
+
+	return (1);
+}
+
+/**
+ * str_len - Calculate the lenght of a string
+ *
+ * @str: String
+ * Return: The lengtht of the string
+ */
+int str_len(char *str)
+{
+	int len;
+
+	for (len = 0; str[len]; len++)
+		;
+
+	return (len);
+}
