0e9e07346d33c26578484ac2d490035ed7163dd2
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-21 08:42:35 -0700

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+
+# Compile all .c files into object files
+gcc -c -Wall -Werror -fpic *.c
+
+# Create the dynamic library liball.so from the object files
 gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
+
+# Print a success message
+echo "Dynamic library liball.so created successfully!"
