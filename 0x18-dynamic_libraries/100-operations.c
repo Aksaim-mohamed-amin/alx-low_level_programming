15be92eb2b8cee257832e0202d0f0919c5140f55
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-21 13:07:37 -0700

    task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include "operations.h"
+
+/**
+ * add - Add two numbers.
+ * @a: First number.
+ * @b: Secend number.
+ *
+ * Return: a + b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Substract two numbers.
+ * @a: First number.
+ * @b: Secend number.
+ *
+ * Return: a - b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiply two numbers.
+ * @a: First number.
+ * @b: Secend number.
+ *
+ * Return: a * b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divisie two numbers.
+ * @a: First number.
+ * @b: Secend number.
+ *
+ * Return: a / b.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Calculate the modular of two numbers.
+ * @a: First number.
+ * @b: Secend number.
+ *
+ * Return: a % b.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef OPERATIONRS_H
+#define OPERATIONRS_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* OPERATIONS_H */
