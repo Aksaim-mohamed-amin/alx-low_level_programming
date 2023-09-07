67d79b1321b387556e9f1359c6f44684300a9d9b
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-07 11:30:51 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,32 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
+
+	if (size < 1)
+	{
+		perror("Siaze of the hash table must be greater than 0\n");
+		return (NULL);
+	}
 
 	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		perror("Error allocating memory for the hash table\n");
+		return (NULL);
+	}
+
 	table->size = size;
-	table->array = NULL;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		perror("Error allocating memory for the array");
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
