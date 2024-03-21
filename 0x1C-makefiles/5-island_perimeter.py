76b607d14065e4f2d4121366328c515016bc9561
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-03-20 18:45:06 -0700

    task 6

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,20 +3,24 @@
 This module contains a function to calculate the perimeter of an island
 """
 
+
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in grid.
 
     Args:
-        grid (List[List[int]]): A list of lists of integers representing the grid.
+        grid (List[List[int]]): A list of lists of integers
+                                representing the grid.
 
     Returns:
         int: The perimeter of the island.
 
     Constraints:
         - Grid is rectangular, width and height don’t exceed 100.
-        - Grid is completely surrounded by water, and there is one island (or nothing).
-        - The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+        - Grid is completely surrounded by water,
+          and there is one island (or nothing).
+        - The island doesn’t have “lakes” (water inside that isn’t connected
+          to the water around the island).
     """
     perimeter = 0
 
