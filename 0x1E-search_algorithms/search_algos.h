f58f9fd02895b91407b95ce19ec35b1e3ec1bd4b
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-07-17 13:44:35 -0700

    task 0: Linear search algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Main Functions */
+int linear_search(int *array, size_t size, int value);
+
+/* Help Functions */
+
+
+#endif /* SEARCH_ALGOS_H */
